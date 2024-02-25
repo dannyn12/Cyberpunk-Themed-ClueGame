074d52d663c8a5c3fe80c79ecba36d9063400e41
Danny Nguyen <dbnguyen@mines.edu>
2024-02-25 01:42:05 -0700

    updated target tests

--- src/tests/BoardTestsExp.java
+++ src/tests/BoardTestsExp.java
@@ -18,7 +18,7 @@ public class BoardTestsExp {
 	}
 	
 	/*
-	 * Test adjacencies for edges and center
+	 * Test adjacency for edges and center
 	 */
 	@Test
 	public void testAdjacency() {
@@ -60,6 +60,83 @@ public class BoardTestsExp {
 		Assert.assertTrue(testList5.contains(board.getCell(2, 1)));
 		Assert.assertTrue(testList5.contains(board.getCell(2, 3)));
 		Assert.assertEquals(4, testList5.size());		
+	}
+	
+	/*
+	 * Test targets with rolls and start location
+	 */
+	@Test
+	public void testTargetsNormal() {
+		// test when roll is 1
+		TestBoardCell rollOf1 = board.getCell(0, 0);
+		board.calcTargets(rollOf1, 1);
+		Set<TestBoardCell> targets = board.getTargets();
+		Assert.assertEquals(2, targets.size());
+		Assert.assertTrue(targets.contains(board.getCell(0, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 0)));
+		
+		// test when roll is 2
+		TestBoardCell rollOf2 = board.getCell(0, 0);
+		board.calcTargets(rollOf2, 2);
+		Set<TestBoardCell> targets2 = board.getTargets();
+		Assert.assertEquals(3, targets2.size());
+		Assert.assertTrue(targets2.contains(board.getCell(0, 2)));
+		Assert.assertTrue(targets2.contains(board.getCell(2, 0)));
+		Assert.assertTrue(targets2.contains(board.getCell(1, 1)));
+		
+		// test when roll is 3
+		TestBoardCell rollOf3 = board.getCell(0, 0);
+		board.calcTargets(rollOf3, 3);
+		Set<TestBoardCell> targets3 = board.getTargets();
+		Assert.assertEquals(6, targets3.size());
+		Assert.assertTrue(targets3.contains(board.getCell(3, 0)));
+		Assert.assertTrue(targets3.contains(board.getCell(2, 1)));
+		Assert.assertTrue(targets3.contains(board.getCell(0, 1)));
+		Assert.assertTrue(targets3.contains(board.getCell(1, 2)));
+		Assert.assertTrue(targets3.contains(board.getCell(0, 3)));
+		Assert.assertTrue(targets3.contains(board.getCell(1, 0)));	
+		
+		// test when roll is 4
+		TestBoardCell rollOf4 = board.getCell(0, 0);
+		board.calcTargets(rollOf4, 4);
+		Set<TestBoardCell> targets4 = board.getTargets();
+		Assert.assertEquals(7, targets4.size());
+		Assert.assertTrue(targets3.contains(board.getCell(1, 3)));
+		Assert.assertTrue(targets3.contains(board.getCell(2, 2)));
+		Assert.assertTrue(targets3.contains(board.getCell(3, 1)));
+		Assert.assertTrue(targets3.contains(board.getCell(1, 3)));
+		Assert.assertTrue(targets3.contains(board.getCell(1, 1)));
+		Assert.assertTrue(targets3.contains(board.getCell(0, 2)));
+		Assert.assertTrue(targets3.contains(board.getCell(1, 3)));
+		
+		// test when roll is 5
+		TestBoardCell rollOf5 = board.getCell(0, 0);
+		board.calcTargets(rollOf5, 5);
+		Set<TestBoardCell> targets5 = board.getTargets();
+		Assert.assertEquals(8, targets5.size());
+		Assert.assertTrue(targets5.contains(board.getCell(2, 3)));
+		Assert.assertTrue(targets5.contains(board.getCell(3, 2)));
+		Assert.assertTrue(targets5.contains(board.getCell(2, 3)));
+		Assert.assertTrue(targets5.contains(board.getCell(0, 3)));
+		Assert.assertTrue(targets5.contains(board.getCell(2, 1)));
+		Assert.assertTrue(targets5.contains(board.getCell(0,1)));
+		Assert.assertTrue(targets5.contains(board.getCell(1,0)));
+		Assert.assertTrue(targets5.contains(board.getCell(1,0)));
+		
+		// test when roll is 6
+		TestBoardCell rollOf6 = board.getCell(0, 0);
+		board.calcTargets(rollOf6, 6);
+		Set<TestBoardCell> targets6 = board.getTargets();
+		Assert.assertEquals(9, targets5.size());
+		Assert.assertTrue(targets6.contains(board.getCell(3, 3)));
+		Assert.assertTrue(targets6.contains(board.getCell(2, 2)));
+		Assert.assertTrue(targets6.contains(board.getCell(1, 1)));
+		Assert.assertTrue(targets6.contains(board.getCell(3, 1)));
+		Assert.assertTrue(targets6.contains(board.getCell(2, 0)));
+		Assert.assertTrue(targets6.contains(board.getCell(1, 3)));
+		Assert.assertTrue(targets6.contains(board.getCell(3,0)));
+		Assert.assertTrue(targets6.contains(board.getCell(0,2)));
+		Assert.assertTrue(targets6.contains(board.getCell(0,2)));
 	}
 	
 	
