202c716e60a98730b5dc08cf24a7c2d545e3eacd
Jordan Lam <jordanlam@mines.edu>
2024-03-01 00:26:29 -0700

    added enum vars to DoorDirection, UP, DOWN, LEFT, RIGHT, NONE

--- src/DoorDirection.java
+++ src/DoorDirection.java
@@ -1,7 +1,11 @@
 /*
  * Author: Jordan Lam & Danny Nguyen
- * This class is the DoorDirection class *insert information about DoorDirection*
+ * This class is the DoorDirection class gives direction to where the door is facing
  */
-public class DoorDirection {
-
+public enum DoorDirection {
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT,
+	NONE
 }
