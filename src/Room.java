42e3180cc6a43fc2c29f40beacf3b2225c382941
Jordan Lam <jordanlam@mines.edu>
2024-03-01 00:21:32 -0700

    added Room Constructor and methods for instance variables

--- src/Room.java
+++ src/Room.java
@@ -3,5 +3,37 @@
  * This is the Room class *insert information about room class*
  */
 public class Room {
-
+	private String name;
+	private BoardCell centerCell;
+	private BoardCell labelCell;
+	
+	public Room() {
+		super();
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public BoardCell getCenterCell() {
+		return centerCell;
+	}
+	
+	public void setCenterCell(BoardCell centerCell) {
+		this.centerCell = centerCell;
+	}
+	
+	public BoardCell getLabelCell() {
+		return labelCell;
+	}
+	
+	public void setLabelCell(BoardCell labelCell) {
+		this.labelCell = labelCell;
+	}
+	
+	
 }
