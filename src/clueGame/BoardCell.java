61f2974bdf07c1e3e92bd785d5c1f8ef4ec3cec8
Danny Nguyen <dbnguyen@mines.edu>
2024-03-02 00:31:24 -0700

    added more comments to methods

--- src/clueGame/BoardCell.java
+++ src/clueGame/BoardCell.java
@@ -2,6 +2,7 @@
  * Authors: Jordan Lam, Danny Nguyen
  * BoardCell class this gives information about each invidual cell like if it is a room or is occupied by a player
  * this room also has adjacenyList that shows its adjacent neighbors
+ * 3/2/24
  */
 package clueGame;
 import java.util.HashSet;
