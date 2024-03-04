6d608420a2d9cb802c0d6af0c99dfce7ed366146
Danny Nguyen <dbnguyen@mines.edu>
2024-03-04 01:11:55 -0700

    refactored board class

--- src/clueGame/BadConfigFormatException.java
+++ src/clueGame/BadConfigFormatException.java
@@ -3,12 +3,8 @@
  * This is the BadConfigFormatException class *insert information about class*
  */
 
-//todo
 package clueGame;
 
-<<<<<<< HEAD
-
-=======
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
@@ -37,5 +33,4 @@ public class BadConfigFormatException extends Exception {
 	}
 	
 	
->>>>>>> 7709d7f88a29145671d9959e4272e46c03598910
 }
